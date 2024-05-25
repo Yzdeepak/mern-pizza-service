a2abe84f000694f75eb70625d8cc833b4fe3c0f4
deepakyadav <yzdeepak@gmail.com>
2024-05-25 13:02:12 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res, next) => {
   res.send("Welcome dear");
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
-import { Logger } from "winston";
 import app from "./app";
 import { Config } from "./config";
 import logger from "./config/logger";
 
 const startServer = async () => {
-  app.listen(Config.PORT, () => {
-    logger.info("Listen on port ");
-  });
+  try {
+    app.listen(Config.PORT, () => {
+      logger.info("Listen on port ");
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      logger.error(err.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
+  }
 };
 
 startServer();
