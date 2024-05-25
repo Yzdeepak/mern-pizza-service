9d8929a37706445307b8af343a1677fe5ce51b99
deepakyadav <yzdeepak@gmail.com>
2024-05-25 08:43:28 +0530

    just for fun

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+
+
+// // @ts-check
+// module.exports = {
+//     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+//     parser: '@typescript-eslint/parser',
+//     plugin: ['@typescript-eslint'],
+//     root: true,
+// }
