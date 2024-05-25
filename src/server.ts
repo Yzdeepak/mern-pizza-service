4bd2a5acf9c9fb9f80d732bb0a45b3fbf3fea2ae
deepakyadav <yzdeepak@gmail.com>
2024-05-25 11:57:32 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import { Logger } from "winston";
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = async () => {
   app.listen(Config.PORT, () => {
-    console.log("Listen on port ");
+    logger.info("Listen on port ");
   });
 };
 
