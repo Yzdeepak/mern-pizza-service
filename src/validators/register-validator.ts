f41aacfac9cac86fc0c9f6e100032fff64931b19
deepakyadav <yzdeepak@gmail.com>
2024-05-29 16:36:32 +0530

    email require validation

---
+++ src/validators/register-validator.ts
@@ -0,0 +1,10 @@
+import { body, checkSchema } from "express-validator";
+
+export default checkSchema({
+  email: {
+    errorMessage: "Email is required!",
+    notEmpty: true,
+  },
+});
+
+// export default [body("email").notEmpty().withMessage("Email is required")];
