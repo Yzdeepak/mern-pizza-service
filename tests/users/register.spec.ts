bed7c8a8d43ee6103fbc422561da9d27866f78c8
deepakyadav <yzdeepak@gmail.com>
2024-05-30 15:31:34 +0530

    persist refresh token

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -6,6 +6,7 @@ import { AppDataSource } from "../../src/config/data-source";
 import { User } from "../../src/entity/User";
 import { Roles } from "../../src/constants";
 import { isjwt } from "../utils";
+import { RefreshToken } from "../../src/entity/RefreshToken";
 
 describe("POST /auth/register", () => {
   let connection: DataSource;
@@ -192,8 +193,6 @@ describe("POST /auth/register", () => {
 
       const cookies =
         (response.headers as unknown as Headers)["set-cookie"] || [];
-      console.log(cookies);
-      console.log(response.headers);
 
       cookies.forEach((cookie) => {
         if (cookie.startsWith("accessToken=")) {
@@ -209,6 +208,32 @@ describe("POST /auth/register", () => {
 
       expect(isjwt(accessToken)).toBeTruthy();
       expect(isjwt(refreshToken)).toBeTruthy();
+    });
+
+    it("Should store the refresh token in the database", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Deepak",
+        lastName: "yadav",
+        email: "yzdeepak@gmail.com",
+        password: "secret",
+      };
+
+      // Act
+
+      const response = await request(app).post("/auth/register").send(userData);
+
+      //assert
+      const refreshTokenRepo = connection.getRepository(RefreshToken);
+      // const refreshTokens = await refreshTokenRepo.find();
+      // expect(refreshTokens).toHaveLength(1);
+      const tokens = await refreshTokenRepo
+        .createQueryBuilder("refreshToken")
+        .where("refreshToken.userId = :userId", {
+          userId: (response.body as Record<string, string>).id,
+        })
+        .getMany();
+      expect(tokens).toHaveLength(1);
     });
   });
 
